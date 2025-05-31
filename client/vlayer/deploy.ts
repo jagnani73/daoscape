046110068cc6863e92faee3415443c66bd749c04
Karan <karanpargal007@gmail.com>
2025-06-01 02:22:01 +0530

    chore: comment for buildg

--- client/vlayer/deploy.ts
+++ client/vlayer/deploy.ts
@@ -1,54 +1,54 @@
-import proverSpec from "../out/DynamicTwitterProver.sol/DynamicTwitterProver.json";
-import verifierSpec from "../out/DynamicTwitterVerifier.sol/DynamicTwitterVerifier.json";
-import emailProverSpec from "../out/EmailDomainProver.sol/EmailDomainProver.json";
-import emailVerifierSpec from "../out/EmailProofVerifier.sol/EmailDomainVerifier.json";
-import githubProverSpec from "../out/GitHubProver.sol/GitHubProver.json";
-import githubVerifierSpec from "../out/GitHubVerifier.sol/GitHubVerifier.json";
-import {
-  deployVlayerContracts,
-  getConfig,
-  writeEnvVariables,
-} from "@vlayer/sdk/config";
+// import proverSpec from "../out/DynamicTwitterProver.sol/DynamicTwitterProver.json";
+// import verifierSpec from "../out/DynamicTwitterVerifier.sol/DynamicTwitterVerifier.json";
+// import emailProverSpec from "../out/EmailDomainProver.sol/EmailDomainProver.json";
+// import emailVerifierSpec from "../out/EmailProofVerifier.sol/EmailDomainVerifier.json";
+// import githubProverSpec from "../out/GitHubProver.sol/GitHubProver.json";
+// import githubVerifierSpec from "../out/GitHubVerifier.sol/GitHubVerifier.json";
+// import {
+//   deployVlayerContracts,
+//   getConfig,
+//   writeEnvVariables,
+// } from "@vlayer/sdk/config";
 
-const config = getConfig();
+// const config = getConfig();
+
+// // const { prover, verifier } = await deployVlayerContracts({
+// //   proverSpec,
+// //   verifierSpec,
+// // });
+
+// // const { emailProver, emailVerifier } = await deployVlayerContracts({
+// //   proverSpec: emailProverSpec,
+// //   verifierSpec: emailVerifierSpec,
+// // });
+
+// // console.log("prover", prover);
+// // console.log("verifier", verifier);
+// // console.log("emailProver", emailProver);
+// // console.log("emailVerifier", emailVerifier);
 
 // const { prover, verifier } = await deployVlayerContracts({
-//   proverSpec,
-//   verifierSpec,
-// });
-
-// const { emailProver, emailVerifier } = await deployVlayerContracts({
-//   proverSpec: emailProverSpec,
-//   verifierSpec: emailVerifierSpec,
+//   // @ts-expect-error - Prover Spec is not typed
+//   proverSpec: githubProverSpec,
+//   // @ts-expect-error - Verifier Spec is not typed
+//   verifierSpec: githubVerifierSpec,
 // });
 
 // console.log("prover", prover);
 // console.log("verifier", verifier);
-// console.log("emailProver", emailProver);
-// console.log("emailVerifier", emailVerifier);
 
-const { prover, verifier } = await deployVlayerContracts({
-  // @ts-expect-error - Prover Spec is not typed
-  proverSpec: githubProverSpec,
-  // @ts-expect-error - Verifier Spec is not typed
-  verifierSpec: githubVerifierSpec,
-});
-
-console.log("prover", prover);
-console.log("verifier", verifier);
-
-// await writeEnvVariables(".env", {
-//   VITE_PROVER_ADDRESS: prover,
-//   VITE_VERIFIER_ADDRESS: verifier,
-//   VITE_EMAIL_PROVER_ADDRESS: emailProver,
-//   VITE_EMAIL_VERIFIER_ADDRESS: emailVerifier,
-//   VITE_CHAIN_NAME: config.chainName,
-//   VITE_PROVER_URL: config.proverUrl,
-//   VITE_JSON_RPC_URL: config.jsonRpcUrl,
-//   VITE_CLIENT_AUTH_MODE: config.clientAuthMode,
-//   VITE_PRIVATE_KEY: config.privateKey,
-//   VITE_VLAYER_API_TOKEN: config.token,
-//   VITE_NOTARY_URL: config.notaryUrl,
-//   VITE_WS_PROXY_URL: config.wsProxyUrl,
-//   VITE_GAS_LIMIT: config.gasLimit,
-// });
+// // await writeEnvVariables(".env", {
+// //   VITE_PROVER_ADDRESS: prover,
+// //   VITE_VERIFIER_ADDRESS: verifier,
+// //   VITE_EMAIL_PROVER_ADDRESS: emailProver,
+// //   VITE_EMAIL_VERIFIER_ADDRESS: emailVerifier,
+// //   VITE_CHAIN_NAME: config.chainName,
+// //   VITE_PROVER_URL: config.proverUrl,
+// //   VITE_JSON_RPC_URL: config.jsonRpcUrl,
+// //   VITE_CLIENT_AUTH_MODE: config.clientAuthMode,
+// //   VITE_PRIVATE_KEY: config.privateKey,
+// //   VITE_VLAYER_API_TOKEN: config.token,
+// //   VITE_NOTARY_URL: config.notaryUrl,
+// //   VITE_WS_PROXY_URL: config.wsProxyUrl,
+// //   VITE_GAS_LIMIT: config.gasLimit,
+// // });
--- client/vlayer/prove.ts
+++ client/vlayer/prove.ts
@@ -1,183 +1,183 @@
-import { createVlayerClient } from "@vlayer/sdk";
-import proverSpec from "../out/WebProofProver.sol/WebProofProver.json";
-import verifierSpec from "../out/WebProofVerifier.sol/WebProofVerifier.json";
-import web_proof from "../testdata/web_proof.json";
-import web_proof_invalid_signature from "../testdata/web_proof_invalid_notary_pub_key.json";
-import * as assert from "assert";
-import { encodePacked, keccak256 } from "viem";
+// import { createVlayerClient } from "@vlayer/sdk";
+// import proverSpec from "../../out/WebProofProver.sol/WebProofProver.json";
+// import verifierSpec from "../../out/WebProofVerifier.sol/WebProofVerifier.json";
+// import web_proof from "../../testdata/web_proof.json";
+// import web_proof_invalid_signature from "../../testdata/web_proof_invalid_notary_pub_key.json";
+// import * as assert from "assert";
+// import { encodePacked, keccak256 } from "viem";
 
-import {
-  getConfig,
-  createContext,
-  deployVlayerContracts,
-  writeEnvVariables,
-} from "@vlayer/sdk/config";
+// import {
+//   getConfig,
+//   createContext,
+//   deployVlayerContracts,
+//   writeEnvVariables,
+// } from "@vlayer/sdk/config";
 
-let config = getConfig();
+// let config = getConfig();
 
-const { prover, verifier } = await deployVlayerContracts({
-  // @ts-expect-error - Prover Spec is not typed
-  proverSpec,
-  // @ts-expect-error - Verifier Spec is not typed
-  verifierSpec,
-});
+// const { prover, verifier } = await deployVlayerContracts({
+//   // @ts-expect-error - Prover Spec is not typed
+//   proverSpec,
+//   // @ts-expect-error - Verifier Spec is not typed
+//   verifierSpec,
+// });
 
-await writeEnvVariables(".env", {
-  VITE_PROVER_ADDRESS: prover,
-  VITE_VERIFIER_ADDRESS: verifier,
-});
+// await writeEnvVariables(".env", {
+//   VITE_PROVER_ADDRESS: prover,
+//   VITE_VERIFIER_ADDRESS: verifier,
+// });
 
-config = getConfig();
-const { chain, ethClient, account, proverUrl, confirmations } =
-  createContext(config);
+// config = getConfig();
+// const { chain, ethClient, account, proverUrl, confirmations } =
+//   createContext(config);
 
-if (!account) {
-  throw new Error(
-    "No account found make sure EXAMPLES_TEST_PRIVATE_KEY is set in your environment variables"
-  );
-}
+// if (!account) {
+//   throw new Error(
+//     "No account found make sure EXAMPLES_TEST_PRIVATE_KEY is set in your environment variables"
+//   );
+// }
 
-const twitterUserAddress = account.address;
-const vlayer = createVlayerClient({
-  url: proverUrl,
-  token: config.token,
-});
+// const twitterUserAddress = account.address;
+// const vlayer = createVlayerClient({
+//   url: proverUrl,
+//   token: config.token,
+// });
 
-await testSuccessProvingAndVerification({
-  chain,
-  ethClient,
-  account,
-  confirmations,
-});
+// await testSuccessProvingAndVerification({
+//   chain,
+//   ethClient,
+//   account,
+//   confirmations,
+// });
 
-await testFailedProving({ chain });
+// await testFailedProving({ chain });
 
-async function testSuccessProvingAndVerification({
-  chain,
-  ethClient,
-  account,
-  confirmations,
-}: Required<
-  Pick<
-    ReturnType<typeof createContext>,
-    "chain" | "ethClient" | "account" | "confirmations"
-  >
->) {
-  console.log("Proving...");
+// async function testSuccessProvingAndVerification({
+//   chain,
+//   ethClient,
+//   account,
+//   confirmations,
+// }: Required<
+//   Pick<
+//     ReturnType<typeof createContext>,
+//     "chain" | "ethClient" | "account" | "confirmations"
+//   >
+// >) {
+//   console.log("Proving...");
 
-  const hash = await vlayer.prove({
-    address: prover,
-    functionName: "main",
-    // @ts-expect-error - Prover Spec is not typed
-    proverAbi: proverSpec.abi,
-    args: [
-      {
-        webProofJson: JSON.stringify(web_proof),
-      },
-      twitterUserAddress,
-    ],
-    chainId: chain.id,
-    gasLimit: config.gasLimit,
-  });
-  const result = await vlayer.waitForProvingResult({ hash });
-  const [proof, twitterHandle, address] = result as [string, string, string];
+//   const hash = await vlayer.prove({
+//     address: prover,
+//     functionName: "main",
+//     // @ts-expect-error - Prover Spec is not typed
+//     proverAbi: proverSpec.abi,
+//     args: [
+//       {
+//         webProofJson: JSON.stringify(web_proof),
+//       },
+//       twitterUserAddress,
+//     ],
+//     chainId: chain.id,
+//     gasLimit: config.gasLimit,
+//   });
+//   const result = await vlayer.waitForProvingResult({ hash });
+//   const [proof, twitterHandle, address] = result as [string, string, string];
 
-  console.log("Verifying...");
+//   console.log("Verifying...");
 
-  // Workaround for viem estimating gas with `latest` block causing future block assumptions to fail on slower chains like mainnet/sepolia
-  const gas = await ethClient.estimateContractGas({
-    address: verifier,
-    abi: verifierSpec.abi,
-    functionName: "verify",
-    args: [proof, twitterHandle, address],
-    account,
-    blockTag: "pending",
-  });
+//   // Workaround for viem estimating gas with `latest` block causing future block assumptions to fail on slower chains like mainnet/sepolia
+//   const gas = await ethClient.estimateContractGas({
+//     address: verifier,
+//     abi: verifierSpec.abi,
+//     functionName: "verify",
+//     args: [proof, twitterHandle, address],
+//     account,
+//     blockTag: "pending",
+//   });
 
-  const txHash = await ethClient.writeContract({
-    address: verifier,
-    abi: verifierSpec.abi,
-    functionName: "verify",
-    args: [proof, twitterHandle, address],
-    chain,
-    account,
-    gas,
-  });
+//   const txHash = await ethClient.writeContract({
+//     address: verifier,
+//     abi: verifierSpec.abi,
+//     functionName: "verify",
+//     args: [proof, twitterHandle, address],
+//     chain,
+//     account,
+//     gas,
+//   });
 
-  await ethClient.waitForTransactionReceipt({
-    hash: txHash,
-    confirmations,
-    retryCount: 60,
-    retryDelay: 1000,
-  });
+//   await ethClient.waitForTransactionReceipt({
+//     hash: txHash,
+//     confirmations,
+//     retryCount: 60,
+//     retryDelay: 1000,
+//   });
 
-  console.log("Verified!");
+//   console.log("Verified!");
 
-  const balance = await ethClient.readContract({
-    address: verifier,
-    abi: verifierSpec.abi,
-    functionName: "balanceOf",
-    args: [twitterUserAddress],
-  });
+//   const balance = await ethClient.readContract({
+//     address: verifier,
+//     abi: verifierSpec.abi,
+//     functionName: "balanceOf",
+//     args: [twitterUserAddress],
+//   });
 
-  assert.strictEqual(balance, 1n);
+//   assert.strictEqual(balance, 1n);
 
-  const tokenOwnerAddress = await ethClient.readContract({
-    address: verifier,
-    abi: verifierSpec.abi,
-    functionName: "ownerOf",
-    args: [generateTokenId(twitterHandle)],
-  });
+//   const tokenOwnerAddress = await ethClient.readContract({
+//     address: verifier,
+//     abi: verifierSpec.abi,
+//     functionName: "ownerOf",
+//     args: [generateTokenId(twitterHandle)],
+//   });
 
-  assert.strictEqual(twitterUserAddress, tokenOwnerAddress);
+//   assert.strictEqual(twitterUserAddress, tokenOwnerAddress);
 
-  const tokenURI = await ethClient.readContract({
-    address: verifier,
-    abi: verifierSpec.abi,
-    functionName: "tokenURI",
-    args: [generateTokenId(twitterHandle)],
-  });
+//   const tokenURI = await ethClient.readContract({
+//     address: verifier,
+//     abi: verifierSpec.abi,
+//     functionName: "tokenURI",
+//     args: [generateTokenId(twitterHandle)],
+//   });
 
-  assert.strictEqual(
-    tokenURI,
-    `https://faucet.vlayer.xyz/api/xBadgeMeta?handle=${twitterHandle}`
-  );
-}
+//   assert.strictEqual(
+//     tokenURI,
+//     `https://faucet.vlayer.xyz/api/xBadgeMeta?handle=${twitterHandle}`
+//   );
+// }
 
-async function testFailedProving({
-  chain,
-}: Pick<ReturnType<typeof createContext>, "chain">) {
-  try {
-    const hash = await vlayer.prove({
-      address: prover,
-      functionName: "main",
-      // @ts-expect-error - Prover Spec is not typed
-      proverAbi: proverSpec.abi,
-      args: [
-        {
-          webProofJson: JSON.stringify(web_proof_invalid_signature),
-        },
-        twitterUserAddress,
-      ],
-      chainId: chain.id,
-      gasLimit: config.gasLimit,
-    });
-    await vlayer.waitForProvingResult({ hash });
-    throw new Error("Proving should have failed!");
-  } catch (error) {
-    assert.ok(
-      error instanceof Error,
-      `Invalid error returned: ${error as string}`
-    );
-    assert.equal(
-      error.message,
-      'Preflight failed with error: Preflight: Transaction reverted: ContractError(Revert(Revert("Invalid notary public key")))',
-      `Error with wrong message returned: ${error.message}`
-    );
-    console.log("✅ Done");
-  }
-}
+// async function testFailedProving({
+//   chain,
+// }: Pick<ReturnType<typeof createContext>, "chain">) {
+//   try {
+//     const hash = await vlayer.prove({
+//       address: prover,
+//       functionName: "main",
+//       // @ts-expect-error - Prover Spec is not typed
+//       proverAbi: proverSpec.abi,
+//       args: [
+//         {
+//           webProofJson: JSON.stringify(web_proof_invalid_signature),
+//         },
+//         twitterUserAddress,
+//       ],
+//       chainId: chain.id,
+//       gasLimit: config.gasLimit,
+//     });
+//     await vlayer.waitForProvingResult({ hash });
+//     throw new Error("Proving should have failed!");
+//   } catch (error) {
+//     assert.ok(
+//       error instanceof Error,
+//       `Invalid error returned: ${error as string}`
+//     );
+//     assert.equal(
+//       error.message,
+//       'Preflight failed with error: Preflight: Transaction reverted: ContractError(Revert(Revert("Invalid notary public key")))',
+//       `Error with wrong message returned: ${error.message}`
+//     );
+//     console.log("✅ Done");
+//   }
+// }
 
-function generateTokenId(username: string): bigint {
-  return BigInt(keccak256(encodePacked(["string"], [username])));
-}
+// function generateTokenId(username: string): bigint {
+//   return BigInt(keccak256(encodePacked(["string"], [username])));
+// }
